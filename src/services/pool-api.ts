d1a03c495215cc4202570c8f6bbc393f3281e612
Alex Aylwin <alexaylwin@gmail.com>
2021-12-29 15:43:04 -0500

    Add pool api

---
+++ src/services/pool-api.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { Bet } from '../models/bet';
+import { Pool } from '../models/pool';
+import { UserInfo } from '../models/user';
+import { filterEmptyBets } from '../utils/validators';
+
+const mockResponse = [
+  {
+      "totalPool": 600,
+      "poolid": "Gender",
+      "betsPerOption": [
+          {
+              "bets": 100,
+              "amount": 200,
+              "option": "Boy"
+          },
+          {
+              "bets": 200,
+              "amount": 400,
+              "option": "Girl"
+          }
+      ],
+      "betName": "Gender"
+  }
+]
+
+export const getPools = async (): Promise<Pool[]> => {
+  //return true;
+  const io = axios.create(
+    {
+      baseURL: 'https://t6hendx94f.execute-api.us-east-2.amazonaws.com',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+  //const resp = await io.get('/pools');
+  return new Promise( (v) => v(mockResponse) );
+}
+
