d06e5e15d0a92d9822a6e4dd78ac78eb1a79ca56
Alex Aylwin <alexaylwin@gmail.com>
2021-12-29 17:47:55 -0500

    Add header content

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ import {
 
 export const Header = () => {
   const [faqShow, setFaqShow] = useState(false);
-  const [tipsShow, setTipsShow] = useState(false)
+  const [tipsShow, setTipsShow] = useState(false);
 
   return (
     <Row className="border-bottom border-3 pb-2">
@@ -19,7 +19,9 @@ export const Header = () => {
       </Col>
 
       <Col className="d-flex justify-content-end">
-        <Button className="me-2" onClick={() => setFaqShow(true)}>About</Button>
+        <Button className="me-2" onClick={() => setFaqShow(true)}>
+          About
+        </Button>
         <Faq show={faqShow} onHide={() => setFaqShow(false)}></Faq>
 
         <Button onClick={() => setTipsShow(true)}>Bet Tips</Button>
@@ -48,31 +50,56 @@ const Faq = (props: any) => {
         <p>
           <strong>How are payouts calculated?</strong>
           <br />
-          The payout for a winning bet is calculated by dividing the total prize pool across all
-          winning bets. Bets amounts are split into $2 increments.
+          The payout for a winning bet is calculated by dividing the total prize
+          pool across all winning bets. Bet amounts are divided into $2
+          increments, called shares. The formula used is for an individual's
+          payout is: <br /> <br />
+          <em>
+            (total prize pool) / (# of winning bets) * (your bet amount / 2)
+          </em>
           <br />
           <br />
           <strong>Example:</strong>
           <br />
-          John has placed a <i>$10 bet on "Boy"</i>, James has placed a <i>$6 bet on "Boy"</i>, and Rachel 
-          has placed a <i>$10 bet on "Girl"</i>. The values are then calculated as follows: 
+          John has placed a <i>$10 bet on "Boy"</i>, James has placed a{" "}
+          <i>$6 bet on "Boy"</i>, and Rachel has placed a{" "}
+          <i>$10 bet on "Girl"</i>. The values are then calculated as follows:
           <ul>
-          <li>Total prize pool for 'Gender': $10 + $6 + $10 = $26 </li>
-          <li>John has 5 shares ($10/2) on "Boy", James has 3 shares on "Boy", and Rachel has 5 shares on "Girl"</li>
-          <li>If "Boy" wins
-            <ul>
-              <li>John's payout is:  $26 (total prize pool) / 8 (winning bets) * 5 (John's shares) = <strong>$16.25</strong></li>
-              <li>James's payout is:  $26 (total prize pool) / 8 (winning bets) * 3 (John's shares) = <strong>$7</strong></li>
-              <li>Rachels's payout is <strong>$0</strong></li>
-            </ul>
-          </li>
-          <li>If "Girl" wins
-            <ul>
-              <li>John's payout is <strong>$0</strong></li>
-              <li>James's payout is <strong>$0</strong></li>
-              <li>Rachels's payout is: $26 (total prize pool) / 5 (winning bets) * 5 (Rachel's shares) = <strong>$26</strong></li>
-            </ul>
-          </li>          
+            <li>
+              Total prize pool for 'Gender': $10 + $6 + $10 ={" "}
+              <strong>$26</strong>{" "}
+            </li>
+            <li>
+              If "Boy" wins
+              <ul>
+                <li>
+                  John's payout is: $26 (total prize pool) / 8 (winning bets) *
+                  5 (John's shares) = <strong>$16.25</strong>
+                </li>
+                <li>
+                  James's payout is: $26 (total prize pool) / 8 (winning bets) *
+                  3 (John's shares) = <strong>$9.75</strong>
+                </li>
+                <li>
+                  Rachels's payout is <strong>$0</strong>
+                </li>
+              </ul>
+            </li>
+            <li>
+              If "Girl" wins
+              <ul>
+                <li>
+                  John's payout is <strong>$0</strong>
+                </li>
+                <li>
+                  James's payout is <strong>$0</strong>
+                </li>
+                <li>
+                  Rachels's payout is: $26 (total prize pool) / 5 (winning bets)
+                  * 5 (Rachel's shares) = <strong>$26</strong>
+                </li>
+              </ul>
+            </li>
           </ul>
         </p>
       </Modal.Body>
