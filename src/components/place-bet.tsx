736bd1e1b7a798ff7c1471d7fe2e4ae3efac3c31
Alex Aylwin <alexaylwin@gmail.com>
2022-01-30 10:43:11 -0500

    Add sticky footer

--- src/components/place-bet.tsx
+++ src/components/place-bet.tsx
@@ -10,17 +10,36 @@ export const PlaceBet = (props: {
 }) => {
   const calcTotal = (amounts: number[]): number => {
     return amounts.reduce((p, c) => {
-      if(isNaN(c)) c = 0;
-      return p + c
+      if (isNaN(c)) c = 0;
+      return p + c;
     }, 0);
   };
 
   return (
-    <footer className="border-t-2 border-black">
-      <div>
-        <span>
-          <b>Total: ${calcTotal(props.bets.map((b) => b.amount))}</b>
+    <footer className="
+      border-t-2 border-black 
+      fixed inset-x-0 bottom-0 
+      leading-10
+      grid grid-cols-2 w-full content-center">
+      <div className="flex flex-col place-content-center px-4 py-8">
+        <span className="font-bold text-2xl">
+          Total: ${calcTotal(props.bets.map((b) => b.amount))}
         </span>
+      </div>
+      <div className="flex flex-col place-content-center">
+        <button
+          className="py-2 
+          px-4 
+          rounded 
+          m-3 
+          font-bold 
+          uppercase
+          shadow-md
+          max-w-sm
+          text-white bg-purple-500 hover:bg-purple-700"
+        >
+          Next Bet
+        </button>
       </div>
     </footer>
   );
