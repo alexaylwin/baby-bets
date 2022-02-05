cc9c557086528dd2e0cbdb324275607e92af2a60
Alex Aylwin <alexaylwin@gmail.com>
2022-02-05 12:00:18 -0500

    Update text colours

--- src/components/bet-tile.tsx
+++ src/components/bet-tile.tsx
@@ -10,9 +10,7 @@ export const BetTile = (props: {
   onChangeSelection: (newSelection: string) => void;
   pool: Pool | undefined;
 }) => {
-  const {
-    pool, bet, rowClass, onChangeAmount, onChangeSelection
-  } = props;
+  const { pool, bet, rowClass, onChangeAmount, onChangeSelection } = props;
 
   const [selectedAmount, setSelectedAmount] = useState<number>(0);
   const [selectedOption, setSelectedOption] = useState<number>(-1);
@@ -21,9 +19,11 @@ export const BetTile = (props: {
 
   const options = [...bet.options];
 
-  const betPercentages = pool?.betsPerOption.map( (bo, index) => 
-  {
-    return {opt: bo.option, percent: ((bo.amount / pool.totalPool) * 100).toFixed(2)}
+  const betPercentages = pool?.betsPerOption.map((bo, index) => {
+    return {
+      opt: bo.option,
+      percent: ((bo.amount / pool.totalPool) * 100).toFixed(2),
+    };
   });
 
   const betOptions = options.map((opt, index) => (
@@ -51,29 +51,27 @@ export const BetTile = (props: {
         {opt.label}
       </button>
       <span className="text-xs display-block max-w-sm content-center text-center">
-        Current bets: { betPercentages?.find( (v) => v.opt === opt.name)?.percent }%
+        Current bets: {betPercentages?.find((v) => v.opt === opt.name)?.percent}
+        %
       </span>
     </div>
   ));
 
   return (
     <div className="grid grid-rows-1 place-items-center px-4 w-11/12">
-      <h2 className="font-bold text-white py-6 text-2xl">
-        {bet.description}
-      </h2>
+      <h2 className="font-bold text-white py-6 text-2xl">{bet.description}</h2>
       <div className="grid grid-cols-2 w-full">{betOptions}</div>
 
       <div className="grid grid-cols-2 place-items-center w-full mt-10 ">
-
         <div className="flex flex-wrap items-stretch w-28 relative">
           <div className="flex -mr-px">
-            <span className="font-bold text-white bg-gray-700 -tracking-normal flex items-center leading-normal rounded rounded-r-none border border-r-0 border-gray-500 px-3 whitespace-no-wrap text-sm">
+            <span className="font-bold text-white bg-gray-700 -tracking-normal flex items-center leading-normal rounded rounded-r-none border border-r-0 border-gray-100 px-3 whitespace-no-wrap text-sm">
               $
             </span>
           </div>
           <input
             type="number"
-            className="flex-shrink flex-grow flex-auto leading-normal w-px border h-10 border-gray-500 rounded rounded-l-none px-3 relative focus:border-blue focus:shadow"
+            className="bg-purple-400 flex-shrink flex-grow flex-auto leading-normal w-px border h-10 border-gray-100 text-gray-100 placeholder-gray-100/50 rounded rounded-l-none px-3 relative focus:border-blue focus:shadow"
             placeholder="Amount"
             onChange={(e) => {
               onChangeAmount(Number.parseInt(e.target.value));
@@ -84,8 +82,8 @@ export const BetTile = (props: {
 
         <span
           className="w-42
-        border border-solid border-white rounded 
-        px-3 py-1.5 text-white font-bold"
+        border border-solid border-gray-100 rounded 
+        px-3 py-1.5 text-gray-100 font-bold"
         >
           Est. Payout: $
           {calcEstimatedPayout(
